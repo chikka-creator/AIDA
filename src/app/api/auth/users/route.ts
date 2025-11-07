d246432c333d4cc6510d4c226823f763621cb776
StallPlayz <stallplayz123@gmail.com>
2025-11-07 22:19:11 +0700

    Memperbaiki koneksi dan mencari jalan alternatif untuk connect ke database IPv6 menggunakan IPv4, dan menyelesaikan database di supabase

---
+++ src/app/api/auth/users/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/src/lib/prisma";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const user = await prisma.user.create({ data: body });
+  return NextResponse.json(user);
+}
